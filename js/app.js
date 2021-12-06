1a5fbd1ad1ce5076b42c97dd945cbf928deb0624
insegas <94861803+insegas@users.noreply.github.com>
2021-12-05 22:50:03 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-if ('serviceWorker' in navigator)	{
-	window.addEventListener('load', () => {
-		navigator.serviceWorker.register('../sw.js').then(() => {
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/service-worker.js');
 			console.log('soy el service Worker registrado y funcionando.')
 		})
 	})
