6b8923e7fd3b0e1ec0d906866e8de1da9f7aeb2b
insegas <94861803+insegas@users.noreply.github.com>
2021-12-05 22:57:44 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 if ('serviceWorker' in navigator)	{
 	window.addEventListener('load', () => {
-		navigator.userAgentData
 		navigator.serviceWorker.register('../sw.js').then(() => {
 			console.log('soy el service Worker registrado y funcionando.')
 		})
