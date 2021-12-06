26df76f7014848e7b2bdcadd7776d2f709635c54
insegas <94861803+insegas@users.noreply.github.com>
2021-12-05 22:20:46 -0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,7 @@
+if ('serviceWorker' in navigator)	{
+	window.addEventListener('load', () => {
+		navigator.serviceWorker.register('../sw.js').then(() => {
+			console.log('soy el service Worker registrado y funcionando.')
+		})
+	})
+}
