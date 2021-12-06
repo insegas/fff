2a8938dc1afdad6dc631b05adde115a6d572d6c8
insegas <94861803+insegas@users.noreply.github.com>
2021-12-05 22:58:31 -0600

    Delete app.js

--- js/app.js
+++
@@ -1,8 +0,0 @@
-if ('serviceWorker' in navigator)	{
-	window.addEventListener('load', () => {
-		navigator.serviceWorker.register('../sw.js').then(() => {
-			console.log('soy el service Worker registrado y funcionando.')
-		})
-	})
-}
-
